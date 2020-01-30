fdc47c2839b7298708e01bdfec79961ace744b76
Bastien <pma.Bastien.aubry@gmail.com>
2020-01-30 10:57:38 +0100

    crud article

---
+++ src/Entity/Article.php
@@ -0,0 +1,58 @@
+<?php
+
+
+namespace App\Entity;
+use Doctrine\ORM\Mapping as ORM;
+/**
+ * Class Article
+ * @package App\Entity
+ * @ORM\Entity
+ */
+class Article
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer", nullable=false)
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=250, nullable=false)
+     */
+    private $nom;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * @param mixed $nom
+     */
+    public function setNom($nom): void
+    {
+        $this->nom = $nom;
+    }
+
+
+}
