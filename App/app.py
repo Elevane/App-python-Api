ca475633f17c64b9019913a5391fa8852f33a7d6
Elevane <pma.bastien.aubry@gmail.com>
2020-06-12 22:45:43 +0200

    post, getall csv

--- App/app.py
+++ App/app.py
@@ -1,14 +1,57 @@
 from flask import Flask, escape, request, jsonify
+import csv
 
 app = Flask(__name__)
 objets = []
+
+
+def checkifExistObjet(object):
+
+    with open('o.csv', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            if row['id'] == object['id']:
+                print(row['id'] , object['id'])
+                return False
+        return True
+
+
+def addObject(object):
+    with open('o.csv', 'w', newline='') as csvfile:
+        fieldnames = ['id', 'test']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        writer.writeheader()
+        writer.writerow({'id': object['id'], 'test': object['test']})
+
+def getAll():
+    o = []
+    with open('o.csv', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            o.append(row)
+        return o
 
 
 @app.route('/objet', methods=['POST'])
 def postObjet():
     if request.is_json:
         content = request.get_json()
-        return jsonify(content)
+        if content['id']:
+            if checkifExistObjet(content):
+                addObject(content)
+                return jsonify(content)
+            return jsonify("Object already exist")
+
+        else:
+            return jsonify("Object given has no Id")
+    else:
+        return jsonify("Invalid Json given")
+
+
+@app.route('/objet', methods=['GET'])
+def getObjet():
+    return jsonify(getAll())
 
 
 if __name__ == '__main__':
